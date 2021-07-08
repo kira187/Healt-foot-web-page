9243f648bab9b98e0937273bea7619d3e3e3ae04
kira187 <missael133@gmail.com>
2021-07-08 09:34:13 -0500

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'first-color': '#069c54',
+        'first-color-alt': '#048654',
+        'title-color': '#393939',
+        'text-color': '#707070',
+        'text-color-light': '#A6A6A6',
+        'body-color': '#FBFEFD',
+        'container-color': '#FFFFFF',
+      },
+      lineHeight: {
+        'extra-loose': '2.5',
+        '12': '3rem',
+      }
+    },
+    fontFamily: {
+      sans: ['"Poppins"', 'sans-serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
